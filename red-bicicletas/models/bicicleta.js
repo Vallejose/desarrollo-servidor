20e197cebc45dd45f1a8eada89c7f9bd684d3bea
Sebastian Vallejo <vallejoseba6@gmail.com>
2020-11-16 19:48:39 -0300

    se crea formulario de bicicletas, agregar, eliminar y editar objetos

---
+++ red-bicicletas/models/bicicleta.js
@@ -0,0 +1,43 @@
+const { bicicleta_create_get } = require("../controllers/bicicleta");
+
+var Bicicleta = function(id, color, modelo, ubicacion) {
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.prototype.toString = function() {
+    return 'd: ' + this.id + "| color: " + this.color;
+
+}
+
+Bicicleta.allBicis = [];
+Bicicleta.add = function(aBici) {
+    Bicicleta.allBicis.push(aBici);
+}
+
+Bicicleta.findById = function(aBiciId) {
+    var aBici = Bicicleta.allBicis.find(z => z.id == aBiciId);
+    if (aBici)
+        return aBici;
+    else
+        throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
+}
+
+Bicicleta.removeById = function(aBiciId) {
+    for (var i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciId) {
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
+var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424, -58.3861497]);
+var b = new Bicicleta(2, 'blanca', 'urbana', [-34.596932, -58.3808287]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
